47b24e5672532708a76edda808c6cdbeda756b80
alsotang <alsotang@gmail.com>
2014-10-12 17:19:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var server = express();
+
+var procressDir = process.cwd();
+
+server.get('/', function (req, res) {
+  res.redirect('/index.html');
+});
+
+server.use(express.static(procressDir));
+
+
+server.listen(5000, function () {
+  console.log('static server is listening at port http://localhost:5000');
+  console.log('static dir is ' + procressDir);
+});
