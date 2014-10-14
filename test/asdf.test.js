ab5e95a4d644951ffb57e836bc82e11df69b5858
alsotang <alsotang@gmail.com>
2014-10-14 20:59:17 +0800

    add test

---
+++ test/asdf.test.js
@@ -0,0 +1,8 @@
+var should = require('should');
+var superagent = require('superagent');
+
+describe('test/adsf/test.js', function () {
+  it('should be ok', function () {
+    'ok'.should.be.ok;
+  });
+});
